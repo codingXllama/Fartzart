3ff08ae640310579d8f13729ba8c31fcf0ed3272
codingXllama <osamaalchalabi2@gmail.com>
2020-05-09 15:18:53 -0400

    created the key fx feature

---
+++ keys.js
@@ -0,0 +1,25 @@
+
+function playFX(evt) {
+  // checking if the key pressed is within the key code we have avaliable
+  const myAudio = document.querySelector(
+    `audio[data-key="${evt.keyCode}"]`
+  );
+  const soundKey = document.querySelector(`.box[data-key="${evt.keyCode}"]`);
+  if (!myAudio) return;
+
+  // resetting the audio time so we can play it again and again
+  myAudio.currentTime = 0;
+  myAudio.play();
+  soundKey.classList.add("isOn");
+}
+
+
+function removeFX(evt) {
+    if (evt.propertyName !== 'transform') return;
+    evt.target.classList.remove("isOn");
+  }
+
+const fxKeys =  Array.from(document.querySelectorAll('.box'));
+fxKeys.forEach(soundKey => soundKey.addEventListener('transitionend', removeFX));
+window.addEventListener("keydown", playFX);
+
